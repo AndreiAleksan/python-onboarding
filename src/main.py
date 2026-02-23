e83f81b80ab1ebc29b5fb54c1331b6a363a1f059
Andrei_Aleksan <andreiaragaoaleks@gmail.com>
2026-02-23 18:05:30 -0300

    code refactored

--- src/main.py
+++ src/main.py
@@ -1,25 +1,31 @@
-file_path = input("Please insert the path of the .txt file you want to run this script with.\n")
 
 
-lines_list = []
-characters_list = []
-total_characters = 0
-total_words = 0
+def wlc_counter(file_name): 
 
-with open(file_path, "r") as file:
-    lines_total = file.readlines()
-    for line in lines_total:
-        lines_list.append(line.strip())
-    for line in lines_list:
-        characters_list.append(len(line))
-        total_characters = sum(characters_list)
-    for line in lines_list:
-        for word in line.split():
-            total_words += 1
-    print(lines_list)
-    print(f"\nTotal words: {total_words}")
-    print(f"Total characters: {total_characters}")
-    print(f"Total lines: {len(lines_list)}")
+    lines_list = []
+    characters_list = []
+    total_characters = 0
+    total_words = 0
 
+    with open(file_name, "r") as file:
+        lines_total = file.readlines()
+        for line in lines_total:
+            lines_list.append(line.strip())
+        for line in lines_list:
+            characters_list.append(len(line))
+            total_characters = sum(characters_list)
+        for line in lines_list:
+            for word in line.split():
+                total_words += 1
+        print(lines_list)
+        print(f"\nTotal words: {total_words}")
+        print(f"Total characters: {total_characters}")
+        print(f"Total lines: {len(lines_list)}")
 
-# C:\Users\Len\Documents\teste.txt
+def main():
+    file_path = input("Please insert the path of the .txt file you want to run this script with.\n")
+    wlc_counter(file_path)
+
+if __name__ == "__main__":
+    main()
+    # C:\Users\Len\Documents\teste.txt
